a58f8d0a381c417cfa64a876a93ac59853b67e5c
kassimhanzouli1-pixel <kassimhanzouli1@gmail.com>
2026-06-10 15:27:46 +0200

    affichage des traits gras entre motifs

--- vue/grille_widget.py
+++ vue/grille_widget.py
@@ -11,6 +11,42 @@ class GrilleWidget(QWidget):
         self.layout_grille.setSpacing(0)
         self.setLayout(self.layout_grille)
 
+    def _trouver_motif(self, grille, l, c):
+        # renvoie le motif auquel appartient la case (l, c)
+        for motif in grille.motifs:
+            if grille.cases[l][c] in motif.cases:
+                return motif
+        return None
+
+    def _construire_bordures(self, grille, l, c):
+        # trait epais (3px) si la case voisine est dans un autre motif ou si c'est le bord de la grille
+        motif_actuel = self._trouver_motif(grille, l, c)
+
+        if l == 0 or self._trouver_motif(grille, l - 1, c) != motif_actuel:
+            haut = 3
+        else:
+            haut = 1
+
+        if l == grille.nb_lignes - 1 or self._trouver_motif(grille, l + 1, c) != motif_actuel:
+            bas = 3
+        else:
+            bas = 1
+
+        if c == 0 or self._trouver_motif(grille, l, c - 1) != motif_actuel:
+            gauche = 3
+        else:
+            gauche = 1
+
+        if c == grille.nb_colonnes - 1 or self._trouver_motif(grille, l, c + 1) != motif_actuel:
+            droite = 3
+        else:
+            droite = 1
+
+        return (f"border-top: {haut}px solid black;"
+                f"border-bottom: {bas}px solid black;"
+                f"border-left: {gauche}px solid black;"
+                f"border-right: {droite}px solid black;")
+
     def afficher_grille(self, grille):
         # supprimer les boutons de l'affichage precedent
         for ligne_boutons in self.boutons:
