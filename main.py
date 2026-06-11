a15ac5c4d0580727e16c2555d28ac4b7d2d61a9a
isthatkiiw <kylianethomas59@gmail.com>
2026-06-11 09:59:55 +0200

    remplacement du main temporaire par le main definitif

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 # Point d'entree du programme
 import sys
 from PyQt6.QtWidgets import QApplication
-from vue.fenetre import FenetrePrincipale
-
-# controleur temporaire pour tester la vue sans le vrai controleur
-class ControleurProvisoire:
-    def charger_grille(self, chemin):
-        print("charger :", chemin)
-    def sauvegarder_grille(self, chemin):
-        print("sauvegarder :", chemin)
-    def verifier(self):
-        print("verifier")
-    def resoudre(self):
-        print("resoudre")
-    def recommencer(self):
-        print("recommencer")
+from controleur.controleur import Controleur
 
 app = QApplication(sys.argv)
-controleur = ControleurProvisoire()
-fenetre = FenetrePrincipale(controleur)
-fenetre.show()
+controleur = Controleur()
+controleur.fenetre.show()
 sys.exit(app.exec())
